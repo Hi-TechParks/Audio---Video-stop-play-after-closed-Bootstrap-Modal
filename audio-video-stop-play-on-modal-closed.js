75cd9fecad58765576cc4f8c3ec3080a6f4d36f4
Md Habibur Rahman <mhrahman@biziitech.com>
2019-01-09 15:52:14 +0600

    Update with Comments

--- audio-video-stop-play-on-modal-closed.js
+++ audio-video-stop-play-on-modal-closed.js
@@ -1,4 +1,22 @@
+
 // Youtube Video Iframe Stop Playing on Bootstrap modal closed
 $("#videoModal").on('hidden.bs.modal', function (e) {
+
     $("#videoModal iframe").attr("src", $("#videoModal iframe").attr("src"));
 });
+
+
+
+// Audio Player Stop Playing on Bootstrap modal closed
+// NB: You can use this same code for HTML video Player also
+$('#myModal').on('hide.bs.modal', function () { //Change #myModal with your modal id
+      $('audio').each(function(){
+        this.pause(); // Stop playing
+        this.currentTime = 0; // Reset time
+      }); 
+})
+// This Script for HTML audio and video palyer
+
+
+
+// You have to go with dynamic modal selector for use it in dynamic content
